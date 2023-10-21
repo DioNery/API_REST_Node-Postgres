7d9919141b2476c678ce44665512b3345710b0f1
Diego Nery <79265846+DioNery@users.noreply.github.com>
2023-10-21 20:08:59 +0000

    atualizações

--- express/index.js
+++ express/index.js
@@ -1,13 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
-const port = 3000;
-const UserCurriculo = require('./models/userCurriculos'); 
+const port = 3001;
 require('dotenv').config();
 
 // Configuração do Sequelize
 const sequelize = new Sequelize({
-  dialect: 'postgres', 
+  dialect: 'postgres',
   host: process.env.BANCO_URL,
   username: process.env.USUARIO_BD,
   password: process.env.SENHA_BD,
@@ -15,87 +14,43 @@ const sequelize = new Sequelize({
   port: 5432,
 });
 
-const userData = {
-    username: 'Usuario1',
-    TextoCurriculo: 'Texto de exemplo do currículo',
-  };
-  
-  UserCurriculo.createUser(userData)
-    .then((user) => {
-      console.log('Novo usuário criado:', user);
-    })
-    .catch((error) => {
-      console.error('Erro ao criar usuário:', error);
-    });
-  
-
-// Sincronize o modelo com o banco de dados
-sequelize.sync();
+// Importar o modelo após criar a instância do sequelize
+const UserCurriculo = require('./models/userCurriculos')(sequelize);
 
 // Middleware para análise de corpo JSON
 app.use(express.json());
 
-// Rota para listar todos os currículos
-app.get('/curriculos', async (req, res) => {
+// Sincronize o modelo com o banco de dados e inicie o servidor depois
+(async () => {
   try {
-    const curriculos = await UserCurriculo.findAllUsers()
-    .then((users) => {
-      console.log('Todos os usuários:', users);
-    })
-    .catch((error) => {
-      console.error('Erro ao encontrar todos os usuários:', error);
-    });
-    res.json(curriculos);
+    await sequelize.authenticate();
+    await sequelize.sync();
+    startServer();
+  } catch (error) {
+    console.error('Erro na conexão com o banco de dados:', error);
+  }
+})();
+
+function startServer() {
+  // Rota para listar todos os currículos
+  app.get('/api/users', async (req, res) => {
+    try {
+      const curriculos = await UserCurriculo.findAllUsers();
+      res.json(curriculos);
+    } catch (error) {
+      console.error('Erro ao listar currículos:', error);
+      res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+  });
+}
+
+app.get('/', async (req, res) => {
+  try {
+    const curriculos = await UserCurriculo.findAllUsers();
+    // res.json(curriculos);
+    res.sendFile(__dirname + '/index.html')
   } catch (error) {
     console.error('Erro ao listar currículos:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
-
-// Rota para criar um novo currículo
-app.post('/curriculos', async (req, res) => {
-  const { nome, experiencia } = req.body;
-  try {
-    const curriculo = await Curriculo.create({ nome, experiencia });
-    res.status(201).json(curriculo);
-  } catch (error) {
-    console.error('Erro ao criar currículo:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
-
-// Rota para atualizar um currículo
-app.put('/curriculos/:id', async (req, res) => {
-  const id = req.params.id;
-  const { nome, experiencia } = req.body;
-  try {
-    const [rowCount] = await Curriculo.update(
-      { nome, experiencia },
-      { where: { id } }
-    );
-    if (rowCount === 0) {
-      res.status(404).json({ error: 'Currículo não encontrado' });
-    } else {
-      res.json({ message: 'Currículo atualizado com sucesso' });
-    }
-  } catch (error) {
-    console.error('Erro ao atualizar currículo:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
-
-// Rota para excluir um currículo
-app.delete('/curriculos/:id', async (req, res) => {
-  const id = req.params.id;
-  try {
-    const rowCount = await Curriculo.destroy({ where: { id } });
-    if (rowCount === 0) {
-      res.status(404).json({ error: 'Currículo não encontrado' });
-    } else {
-      res.json({ message: 'Currículo excluído com sucesso' });
-    }
-  } catch (error) {
-    console.error('Erro ao excluir currículo:', error);
     res.status(500).json({ error: 'Erro interno do servidor' });
   }
 });
--- express/models/userCurriculos.js
+++ express/models/userCurriculos.js
@@ -1,19 +1,8 @@
 const { Sequelize, DataTypes } = require('sequelize');
-require('dotenv').config();
 
-// Configuração do Sequelize
-const sequelize = new Sequelize({
-  dialect: 'postgres', 
-  host: process.env.BANCO_URL,
-  username: process.env.USUARIO_BD,
-  password: process.env.SENHA_BD,
-  database: process.env.DATABASE_BD,
-  port: 5432,
-});
+module.exports = (sequelize) => {
+  const UserCurriculo = sequelize.define("userCurriculo", {
 
-//Criação de Modelo do Usuario
-const getUserModel = (sequelize, { DataTypes }) => {
-    const UserCurriculo = sequelize.define("userCurriculo", {
       username: {
         type: DataTypes.STRING,
         unique: true,
@@ -22,6 +11,7 @@ const getUserModel = (sequelize, { DataTypes }) => {
           notEmpty: true,
         },
       },
+
       TextoCurriculo: {
         type: DataTypes.STRING,
         unique: true,
@@ -30,46 +20,37 @@ const getUserModel = (sequelize, { DataTypes }) => {
           notEmpty: true,
         },
       },
-    });
-  
-    // Associação de tabelas
-    UserCurriculo.associate = (models) => {
-      UserCurriculo.hasMany(models.TextoCurriculo, { onDelete: "CASCADE" });
-    };
-  
-    // Encontrar pelo Login
-    UserCurriculo.findByLogin = async (login) => {
-      let user = await UserCurriculo.findOne({
-        where: { username: login },
-      });
-      return user;
-    };
-  
-    // Método para criar um novo usuário
-    UserCurriculo.createUser = async (userData) => {
-      try {
-        const user = await UserCurriculo.create(userData);
-        return user;
-      } catch (error) {
-        console.error('Erro ao criar usuário:', error);
-        throw error;
-      }
-    };
+  }
+);
 
-    // Método para encontrar todos os usuários
-    UserCurriculo.findAllUsers = async () => {
-        try {
-        const users = await UserCurriculo.findAll();
-        return users;
-        } catch (error) {
-        console.error('Erro ao encontrar todos os usuários:', error);
-        throw error;
-        }
-    };
-  
-  
-    return UserCurriculo;
+  UserCurriculo.createUser = async (username, TextoCurriculo) => {
+    try {
+      const newUser = await UserCurriculo.create({
+        username: username,
+        TextoCurriculo: TextoCurriculo,
+      });
+      return newUser;
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error);
+      throw error;
+    }
   };
-  
-  export default getUserModel;
-  
+
+  // Associação de tabelas
+  UserCurriculo.associate = (models) => {
+    UserCurriculo.hasMany(models.TextoCurriculo, { onDelete: "CASCADE" });
+  };
+
+  // Encontrar todos os usuários
+  UserCurriculo.findAllUsers = async () => {
+    try {
+      const users = await UserCurriculo.findAll();
+      return users;
+    } catch (error) {
+      console.error('Erro ao encontrar todos os usuários:', error);
+      throw error;
+    }
+  };
+
+  return UserCurriculo;
+};
