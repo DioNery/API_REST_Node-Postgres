ff88f8607b5b82e2b955e92a5f9271a4e5288ea7
Diego Nery <79265846+DioNery@users.noreply.github.com>
2023-10-24 22:44:26 +0000

    .

--- express/index.js
+++ express/index.js
@@ -1,7 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const port = process.env.PORT || 3001;
 require('dotenv').config();
 
@@ -17,7 +16,10 @@ const sequelize = new Sequelize({
 
 // Importar o modelo após criar a instância do sequelize
 const UserCurriculo = require('./models/userCurriculos')(sequelize);
+
+// Middleware para análise de corpo JSON
 app.use(express.json());
+
 // Sincronize o modelo com o banco de dados e inicie o servidor depois
 (async () => {
   try {
@@ -29,84 +31,86 @@ app.use(express.json());
   }
 })();
 
+function startServer() {
   // Rota para listar todos os currículos
-  app.get('/', async (req, res) => {
-    try {
-      res.redirect('/users')
-    } catch (error) {
-      console.error('Erro ao listar currículos:', error);
-      res.status(500).json({ error: 'Erro interno do servidor' });
+  app.get('/', (req,res) =>
+  {
+    res.redirect('/users')
+  })
+}
+app.get('/users', async (req, res) => {
+  try {
+    const curriculos = await UserCurriculo.findAllUsers();
+      res.json(curriculos);
+    // } else {
+    //   // Caso contrário, envie o arquivo HTML
+    //   res.sendFile(__dirname + '/index.html');
+    // }
+  } catch (error) {
+    console.error('Erro ao listar currículos:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const { username, TextoCurriculo } = req.body;
+    if (!username || !TextoCurriculo) {
+      return res.status(400).json({ error: 'Campos username e TextoCurriculo são obrigatórios.' });
     }
-  });
-  app.get('/users', async (req, res) => {
-    try {
-      const curriculos = await UserCurriculo.findAllUsers();
-        res.json(curriculos);
-    } catch (error) {
-      console.error('Erro ao listar currículos:', error);
-      res.status(500).json({ error: 'Erro interno do servidor' });
-    }
-  });
 
-// app.post('/users', async (req, res) => {
-//   try {
-//     const { username, TextoCurriculo } = req.body;
-//     if (!username || !TextoCurriculo) {
-//       return res.status(400).json({ error: 'Campos username e TextoCurriculo são obrigatórios.' });
-//     }
+  const newUser = await UserCurriculo.createUser({ username, TextoCurriculo });
 
-//   const newUser = await UserCurriculo.createUser({ username, TextoCurriculo });
-
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     console.error('Erro ao criar usuário:', error);
-//     res.status(500).json({ error: 'Erro interno do servidor' });
-//   }
-// });
-// app.put('/users/:id', async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const { username, TextoCurriculo } = req.body;
+    res.status(201).json(newUser);
+  } catch (error) {
+    console.error('Erro ao criar usuário:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
+app.put('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, TextoCurriculo } = req.body;
     
-//     const user = await UserCurriculo.findOne({ where: { id } });
+    const user = await UserCurriculo.findOne({ where: { id } });
 
-//     if (!user) {
-//       return res.status(404).json({ error: 'Usuário não encontrado' });
-//     }
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
 
-//     // Atualize os dados do usuário
-//     user.username = username;
-//     user.TextoCurriculo = TextoCurriculo;
-//     await user.save();
+    // Atualize os dados do usuário
+    user.username = username;
+    user.TextoCurriculo = TextoCurriculo;
+    await user.save();
 
-//     res.json(user);
-//   } catch (error) {
-//     console.error('Erro ao atualizar usuário:', error);
-//     res.status(500).json({ error: 'Erro interno do servidor' });
-//   }
-// });
-// app.delete('/users/:id', async (req, res) => {
-//   try {
-//     const { id } = req.params;
+    res.json(user);
+  } catch (error) {
+    console.error('Erro ao atualizar usuário:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
 
-//     const user = await UserCurriculo.findOne({ where: { id } });
+    const user = await UserCurriculo.findOne({ where: { id } });
 
-//     if (!user) {
-//       return res.status(404).json({ error: 'Usuário não encontrado' });
-//     }
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
 
-//     await user.destroy();
+    await user.destroy();
 
-//     res.json({ message: 'Usuário excluído com sucesso' });
-//   } catch (error) {
-//     console.error('Erro ao excluir usuário:', error);
-//     res.status(500).json({ error: 'Erro interno do servidor' });
-//   }
-// });
+    res.json({ message: 'Usuário excluído com sucesso' });
+  } catch (error) {
+    console.error('Erro ao excluir usuário:', error);
+    res.status(500).json({ error: 'Erro interno do servidor' });
+  }
+});
 
 
   
 // Iniciar o servidor
-// app.listen(port, () => {
-//   console.log(`Servidor rodando na porta ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
